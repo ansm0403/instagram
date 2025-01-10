7127d40f561f078579c765602177f406def5c631
an-sangmoon <tkdans312@gmail.com>
2025-01-10 22:59:52 +0900

    fixed img error

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,19 @@ const nextConfig = {
         remotePatterns : [
             {
                 protocol : 'https',
+                hostname : 'lh3.googleusercontent.com',
+                port : '',
+                pathname : '/**'
+            },
+            {
+                protocol : 'https',
                 hostname : 'cdn.sanity.io',
+                port : '',
+                pathname : '/**'
+            },
+            {
+                protocol : 'https',
+                hostname : 'images.unsplash.com',
                 port : '',
                 pathname : '/**'
             }
