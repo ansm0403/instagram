822c6fcf68d41f5f038fcd82ae70a75714359cda
an-sangmoon <tkdans312@gmail.com>
2025-01-13 23:23:37 +0900

    fixed env error

--- src/app/env/env.ts
+++ src/app/env/env.ts
@@ -1,3 +1,3 @@
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-export const token = process.env.NEXT_PUBLIC_SANITY_SECRET_TOKEN
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string
+export const token = process.env.NEXT_PUBLIC_SANITY_SECRET_TOKEN as string
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET as string
