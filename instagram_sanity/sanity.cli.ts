e290c7062c9c732c17c703dc454b3825eb91b439
an-sangmoon <tkdans312@gmail.com>
2025-01-15 20:28:43 +0900

    sanity error fixed

--- instagram_sanity/sanity.cli.ts
+++ instagram_sanity/sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+// import {defineCliConfig} from 'sanity/cli'
 
-export default defineCliConfig({
-  api: {
-    projectId: 's4ob0gng',
-    dataset: 'production'
-  }
-})
+// export default defineCliConfig({
+//   api: {
+//     projectId: 's4ob0gng',
+//     dataset: 'production'
+//   }
+// })
